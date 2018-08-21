68a099103099056da01749108a3b9fedaf20e9ef
tkc49 <h.tkc49@gmail.com>
2018-08-21 13:12:09 +0900

    [fix] PHP Notice:  Trying to get property of non-object

--- includes/check-acceptance.php
+++ includes/check-acceptance.php
@@ -8,7 +8,7 @@ function check_acceptance( $value, $cf7_mail_tag ){
 	
 	$mail_tag = new WPCF7_MailTag( '[' . $cf7_mail_tag . ']', $cf7_mail_tag, "" );
 	$form_tag = $mail_tag->corresponding_form_tag();		
-	if($form_tag->type == 'acceptance'){
+	if( isset( $form_tag->type ) && $form_tag->type == 'acceptance'){
 		$value = $form_tag->content;
 	}
 	return $value;
