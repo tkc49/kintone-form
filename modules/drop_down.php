1e9009f409af92b0ad5dcbd60f57d4b901e6159c
tkc49 <h.tkc49@gmail.com>
2019-09-19 17:37:13 +0900

    Change checking of KintoneForm_drop_down.

--- modules/drop_down.php
+++ modules/drop_down.php
@@ -1,75 +1,63 @@
 <?php
 
-class KintoneForm_drop_down
-{
+class KintoneForm_drop_down {
 
 	/*
-	 * get instance
+	 * get instance.
 	 */
-	public static function getInstance()
-	{
+	public static function getInstance() {
 		/**
-		* a variable that keeps the sole instance.
-		*/
+		 * a variable that keeps the sole instance.
+		 */
 		static $instance;
 
-		if ( !isset( $instance ) ) {
+		if ( ! isset( $instance ) ) {
 			$instance = new KintoneForm_drop_down();
 		}
+
 		return $instance;
 	}
 
+	/**
+	 * Format_to_kintone_data
+	 *
+	 * @param array    $kintone_form_data .
+	 * @param array    $cf7_send_data .
+	 * @param string   $cf7_mail_tag .
+	 * @param WP_Error $e .
+	 *
+	 * @return array An array of image URLs.
+	 */
 	public static function format_to_kintone_data( $kintone_form_data, $cf7_send_data, $cf7_mail_tag, $e ) {
 
 		$return_data = array();
 
-        $value = '';
-		if( isset($cf7_send_data[$cf7_mail_tag]) ){
-            $value = $cf7_send_data[$cf7_mail_tag];
-        }
+		$value = '';
+		if ( isset( $cf7_send_data[ $cf7_mail_tag ] ) ) {
+			$value = $cf7_send_data[ $cf7_mail_tag ];
+		}
 
 
 		//
-		// Check Acceptance
+		// Check Acceptance.
 		//
 		$value = kintone_form_check_acceptance( $value, $cf7_mail_tag );
 
-
-		if( is_array($value) ){
+		if ( is_array( $value ) ) {
 			$value = $value[0];
 		}
 
-		if( $kintone_form_data['required'] == 'true' && empty($value) ){
+		if ( 'true' === $kintone_form_data['required'] && empty( $value ) ) {
 
-			$e->add('Error', $cf7_mail_tag .'->'. $kintone_form_data['code'].' : Required fields');
+			$e->add( 'Error', $cf7_mail_tag . '->' . $kintone_form_data['code'] . ' : Required fields' );
 
 		}
-
-		if( !empty( $value ) ){
-
-			$match_flg = false;
-			foreach ($kintone_form_data['options'] as $option_key => $option_value) {
-
-				if( $value == $option_value ){
-					$match_flg = true;
-				}
-
-			}
-			if(!$match_flg){
-				$e->add('Error', $cf7_mail_tag .'->' . $kintone_form_data['code'].' : not match');
-				$e->add('Error', 'kintone form data:' . var_export($kintone_form_data['options'], true));
-				$e->add('Error', 'CF7 value:' . $value);
-
-			}
-		}
-
 
 		$return_data['value'] = $value;
 
 		return $return_data;
 
 	}
-
 
 
 }
