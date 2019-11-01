e3d1a27de661bb8143dd712add6fe85af2b1d032
tkc49 <h.tkc49@gmail.com>
2019-11-01 13:52:49 +0900

    リファクタリング

--- includes/class-kintone-form-KintoneFormUtility.php
+++ includes/class-kintone-form-KintoneFormUtility.php
@@ -2,17 +2,10 @@
 
 class Kintone_Form_Utility {
 	public static function decode( $encoded ) {
-		return preg_match( '/^[a-f0-9]{32}$/', $encoded ) ? $encoded : str_replace(
-			array(
-				md5( AUTH_SALT ),
-				md5( md5( AUTH_SALT ) )
-			),
-			'',
-			base64_decode( $encoded )
-		);
+		return preg_match( '/^[a-f0-9]{32}$/', $encoded ) ? $encoded : str_replace( array( md5( AUTH_SALT ), md5( md5( AUTH_SALT ) ) ), '', base64_decode( $encoded ) );
 	}
 
-	//  form data to kintone WordPress Plugin incorporates code from WP to kintone WordPress Plugin, Copyright 2016 WordPress.org
+	//  form data to kintone WordPress Plugin incorporates code from WP to kintone WordPress Plugin, Copyright 2016 WordPress.org.
 	public static function get_auth_header( $token ) {
 		if ( $token ) {
 			return array( 'X-Cybozu-API-Token' => $token );
