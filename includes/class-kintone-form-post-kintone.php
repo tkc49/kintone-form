3be2eb66e494cf33153864ff7649746f2b65df6a
tkc49 <h.tkc49@gmail.com>
2020-06-23 21:44:03 +0900

    "form_data_to_kintone_kintone_post_url" to Add parameters

--- includes/class-kintone-form-post-kintone.php
+++ includes/class-kintone-form-post-kintone.php
@@ -421,7 +421,7 @@ class Kintone_Form_Post_Kintone {
 			);
 
 			// kintoneにフォームデータを追加/更新する.
-			$url = apply_filters( 'form_data_to_kintone_kintone_post_url', $url );
+			$url = apply_filters( 'form_data_to_kintone_kintone_post_url', $url, $tmp_options );
 			$res = wp_remote_post(
 				$url,
 				$options
