096185a28a5a45b1d677f0737fa104a9bdbb4d62
tkc49 <h.tkc49@gmail.com>
2020-09-02 00:05:52 +0900

    [Remove] Remove the ability to save logged-in user information to kintone.

--- kintone-form.php
+++ kintone-form.php
@@ -3,7 +3,7 @@
  * Plugin Name: Form data to kintone
  * Plugin URI:
  * Description: This plugin is an addon for "Contact Form 7".
- * Version:     2.17.0
+ * Version:     2.17.1
  * Author:      Takashi Hosoya
  * Author URI:  http://ht79.info/
  * License:     GPLv2
